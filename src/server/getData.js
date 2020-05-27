ce7eb4151cfd133f477c2f10f1705053a56e0a35
gowthamaraj <gowthamaraj360@gmail.com>
2020-05-27 16:55:23 +0530

    feat: env variables added and tested.

--- src/server/getData.js
+++ src/server/getData.js
@@ -1,16 +1,20 @@
 //librabry for post and get method in node environment
 const fetch = require("node-fetch");
 
+//for env variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 //define a function which take the data send by the server and call the respective API for the end data.
 const getAllData = async (city,dep,future) => {
     // get from geonames
     const Now = Date.now() * 0.001;
-    const cityRes = await fetch(`http://api.geonames.org/searchJSON?q=${city}&maxRows=2&username=gowthamaraj`);
+    const cityRes = await fetch(`http://api.geonames.org/searchJSON?q=${city}&maxRows=2&username=${process.env.API_GEO}`);
     const cityData = await cityRes.json();
     let country = cityData.geonames[0].countryName;
 
     //get some pics url from pixabay using the city parameter given to the function
-    const imageRes = await fetch(`https://pixabay.com/api/?key=16702619-050e1a8f9aa4137888285d80c&q=${city}+city&image_type=photo`);
+    const imageRes = await fetch(`https://pixabay.com/api/?key=${process.env.API_PIX}&q=${city}+city&image_type=photo`);
     const imageData = await imageRes.json();
 
     let image1,image2,image3;
@@ -43,7 +47,7 @@ const getAllData = async (city,dep,future) => {
     daysLeft = Math.ceil(daysLeft);
     if(daysLeft>16){daysLeft = 15;}
     //get data from the weatherbit api
-    const weatherRes = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${cityData.geonames[0].lat}&lon=${cityData.geonames[0].lng}&key=d475eeb87f5645f8b8fb8d3c2f376033&units=I&days=${daysLeft}`);
+    const weatherRes = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${cityData.geonames[0].lat}&lon=${cityData.geonames[0].lng}&key=${process.env.API_BIT}&units=I&days=${daysLeft}`);
     const weatherData = await weatherRes.json();
 
     // return this data 
